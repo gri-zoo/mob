ed266ed2bc3bfe6dcf2954dd71d315aaaa2c09e7
kabexnuf <kabexxnuf@gmail.com>
2014-07-06 17:34:36 +0100

    Introduce karma
    Start using karma as unit test runner. Currently, two modes are configured
    via grunt.
    
    continous: Can be run with "grunt unit-continuous". Watches files in the
    background and reruns whenever a file changes. To be used in DEV
    environment
    
    single: Can be run with "grunt unit". Runs all unit tests once. To be used
    in CI environment.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,38 @@
 module.exports = function(grunt) {
+  grunt.initConfig({
+    connect: {
+      server: {
+	      options: {
+		      port: 8000,
+		      base: './www',
+		      keepalive: true
+		    }
+	    }
+	  },
+    karma: {
+      continuous: {
+        configFile: 'config/karma.conf.js',
+        background: true
+      },
+      single: {
+        configFile: 'config/karma.conf.js',
+        singleRun: true,
+  },
+    },
+    watch: {
+      karma: {
+        files: ['www/js/**/*.js', 
+          'www/test/unit/**/*.js'
+        ],
+        tasks: ['karma:unit:run']
+      }
+    }
+  });
 
-	grunt.initConfig({
-	  connect: {
-		server: {
-		  options: {
-			port: 8000,
-			base: './www',
-			keepalive: true
-		  }
-		}
-	  }
-	});
-	
-	grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-karma');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  
+  grunt.registerTask('unit-continuous', ['karma:continuous', 'watch']);
+  grunt.registerTask('unit', ['karma:single']);
 };
